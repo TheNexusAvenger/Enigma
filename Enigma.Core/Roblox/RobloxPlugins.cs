46ee0a0c6685ed1a6eadf7fe8e4b24a70c0a53b3
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2024-07-04 15:17:29 -0400

    Fix NullPointerException in published app when copying plugin.

--- Enigma.Core/Roblox/RobloxPlugins.cs
+++ Enigma.Core/Roblox/RobloxPlugins.cs
@@ -34,7 +34,7 @@ public static class RobloxPlugins
     public static async Task CopyPluginAsync()
     {
         // Return if the source plugin does not exist.
-        var sourcePluginPath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)!, SourcePluginName);
+        var sourcePluginPath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location) ?? ".", SourcePluginName);
         if (!File.Exists(sourcePluginPath))
         {
             Logger.Debug($"Source plugin file {sourcePluginPath} not found. The managed plugin will not be copied.");
