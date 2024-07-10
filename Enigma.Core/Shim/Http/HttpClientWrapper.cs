881c43e261098f27531c5cbe4900e8b2600e3c98
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2024-07-10 13:44:00 -0400

    Disable update checker due to Windows Defender problems.

--- Enigma.Core/Shim/Http/HttpClientWrapper.cs
+++ Enigma.Core/Shim/Http/HttpClientWrapper.cs
@@ -1,5 +1,7 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Enigma.Core.Web.GitHub.Model;
 
 namespace Enigma.Core.Shim.Http;
 
@@ -8,7 +10,8 @@ public class HttpClientWrapper : IHttpClient
     /// <summary>
     /// Http client used to send requests.
     /// </summary>
-    private readonly HttpClient _httpClient = new HttpClient();
+    // TODO: Windows Defender is triggered by this.
+    // private readonly HttpClient _httpClient = new HttpClient();
     
     /// <summary>
     /// Sets the user agent for the HTTP client to use.
@@ -16,7 +19,8 @@ public class HttpClientWrapper : IHttpClient
     /// <param name="userAgent">User agent to use.</param>
     public void SetUserAgent(string userAgent)
     {
-        this._httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
+        // TODO: Windows Defender is triggered by this.
+        // this._httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
     }
 
     /// <summary>
@@ -26,6 +30,15 @@ public class HttpClientWrapper : IHttpClient
     /// <returns>Response for the request.</returns>
     public async Task<HttpResponseMessage> GetAsync(string url)
     {
-        return await this._httpClient.GetAsync(url);
+        // TODO: Windows Defender is triggered by this.
+        // return await this._httpClient.GetAsync(url);
+        // To avoid other errors, this dummy code is used.
+        // Currently, GetAsync is only used for the update check.
+        var versionData = await ProjectVersionData.GetVersionDataAsync();
+        return new HttpResponseMessage()
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent($"[{{\"name\":\"{versionData?.Version}\"}}]"),
+        };
     }
 }
